61b0ffa9be8b5d6c90fbf8daf9858b4c184ea812
Marcos Cosio <marcoscosiocollias@gmail.com>
2023-01-18 00:34:34 -0300

    Implementacion de Bootstrap en el esqueleto HTML, ajuste de imagen/Bootstrap implementation on HTML skeleton, image adjustment

--- js/functions.js
+++ js/functions.js
@@ -7,7 +7,26 @@ function getData(url, cb) {
 getData('https://randomuser.me/api/', 
   function (data) {
     console.log( data.results[0] );
-    document.getElementById("portrait").src= data.results[0].picture.medium;
+    //vamos a suponer arbitrariamente que el 35% de la población es no binaria||We are going to suppose arbitrarily that a 35% of population is non-binary
+    if (Math.random()<0.35) 
+    {
+        document.getElementById("portrait").src="./non-binary.jpg";
+        document.getElementById("portrait").alt=data.results[0].name.first+" "+data.results[0].name.last;
+    }
+    else
+    {
+        if (data.results[0].gender==="male")
+         {
+          document.getElementById("portrait").src="./male.jpg";
+          document.getElementById("portrait").alt=data.results[0].name.first+" "+data.results[0].name.last;
+         }
+         else
+         {
+          document.getElementById("portrait").src="./female.jpg";
+          document.getElementById("portrait").alt=data.results[0].name.first+" "+data.results[0].name.last;
+         }
+    }
+    
     document.getElementById("main").removeAttribute("hidden");
   } 
 )  
